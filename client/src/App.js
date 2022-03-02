ea9400b10d76c1ffd2db120246cdba857ca525fa
Akendra-Chaulagain <yakendrachaulagain31@gmail.com>
2022-03-02 16:18:09 +0545

    folder of client and api side created

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+<h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus, harum.</h1>
+    </>
+  )
+}
+
+export default App
