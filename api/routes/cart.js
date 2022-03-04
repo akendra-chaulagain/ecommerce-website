2e2b9aaaa0f95ba0432df4a00c54284c5eaba552
Akendra-Chaulagain <yakendrachaulagain31@gmail.com>
2022-03-04 19:08:51 +0545

    card function added

--- api/routes/cart.js
+++ api/routes/cart.js
@@ -34,64 +34,64 @@ router.post("/", verifyToken, async (req, res) => {
 
 
 // update product 
-// router.put("/:id", verifyToken, async (req, res) => {
-//     if (req.user.id === req.params.id || req.user.isAdmin) {
-//         try {
-//             const cartUpdate = await Cart.findByIdAndUpdate(req.params.id, {
-//                 $set: req.body
-//             }, { new: true })
-//             res.status(201).json(cartUpdate)
-//         } catch (error) {
-//             res.status(401).json(error)
-//         }
-//     } else {
-//         res.status(401).json("Unable yo update  cart")
-//     }
-// })
+router.put("/:id", verifyToken, async (req, res) => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+        try {
+            const cartUpdate = await Cart.findByIdAndUpdate(req.params.id, {
+                $set: req.body
+            }, { new: true })
+            res.status(201).json(cartUpdate)
+        } catch (error) {
+            res.status(401).json(error)
+        }
+    } else {
+        res.status(401).json("Unable yo update  cart")
+    }
+})
 
 // delete product
-// router.delete("/:id", verifyToken, async (req, res) => {
-//     if (req.user.id === req.params.id || req.user.isAdmin) {
-//         try {
-//             const deleteCart = await Cart.findByIdAndDelete(req.params.id)
-//             res.status(201).json(deleteCart)
-//         } catch (error) {
-//             res.status(401).json(error)
-//         }
+router.delete("/:id", verifyToken, async (req, res) => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+        try {
+            const deleteCart = await Cart.findByIdAndDelete(req.params.id)
+            res.status(201).json(deleteCart)
+        } catch (error) {
+            res.status(401).json(error)
+        }
 
-//     } else {
-//         res.status(401).json("You are not allowed to delete other account")
-//     }
+    } else {
+        res.status(401).json("You are not allowed to delete other account")
+    }
 
-// })
+})
 
 
 
 // get individual cart
-// router.get("/find/:id", verifyToken, async (req, res) => {
-//     try {
-//         const getIndividualCart = await Cart.findById(req.params.id)
-//         res.status(200).json(getIndividualCart);
-//     } catch (error) {
-//         res.status(401).json(error)
-//     }
-// })
+router.get("/find/:id", verifyToken, async (req, res) => {
+    try {
+        const getIndividualCart = await Cart.findById(req.params.id)
+        res.status(200).json(getIndividualCart);
+    } catch (error) {
+        res.status(401).json(error)
+    }
+})
 
 
 
 // get all cart
-// router.get("/", verifyToken, async (req, res) => {
-//     if (req.user.id === req.params.id || req.user.isAdmin) {
-//         try {
-//             const allCart = await Cart.find()
-//             res.status(201).json(allCart)
-//         } catch (error) {
-//             res.status(401).json(error)
-//         }
-//     } else {
-//         res.status(401).json("Unable to get all cart")
-//     }
-// })
+router.get("/", verifyToken, async (req, res) => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+        try {
+            const allCart = await Cart.find()
+            res.status(201).json(allCart)
+        } catch (error) {
+            res.status(401).json(error)
+        }
+    } else {
+        res.status(401).json("Unable to get all cart")
+    }
+})
 
 
 
