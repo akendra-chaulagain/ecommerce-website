0db78959b0ef7e53d90bca779f203e6adaf7cfbb
Akendra-Chaulagain <yakendrachaulagain31@gmail.com>
2022-03-06 06:50:01 +0545

    navbar added

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
+import Navbar from '../../components/navbar/Navbar'
 
 const Home = () => {
     return (
         <>
-            <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae quisquam nobis perferendis velit, expedita ea non ullam deleniti doloribus obcaecati rerum quam quos similique nulla quas ut sit commodi quibusdam quo ducimus? Quisquam laboriosam rerum debitis est optio laudantium, sed deserunt atque eum, saepe ut? Vitae cumque quasi magni nulla!</h1>
-
-            <h1>Home</h1>
+          {/* navbar import from navbar components */}
+          <Navbar/>
         </>
     )
 }
