247cd764b9527a4ed7ce014d4f91b8451a65afbd
Akendra-Chaulagain <yakendrachaulagain31@gmail.com>
2022-03-06 10:20:40 +0545

    register page added

---
+++ client/src/components/loginRegisterFooter/LoginregisterFooter.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./LoginregisterFooter.css"
+
+const LoginregisterFooter = () => {
+  return (
+      <>
+          <hr />
+          <div className="loginregisterFooter">
+              <p>© 2021-2022, AllInOne.com, Inc. or its affiliates</p>
+          </div>
+      </>
+    
+  )
+}
+
+export default LoginregisterFooter
--- client/src/pages/register/Register.js
+++ client/src/pages/register/Register.js
@@ -1,42 +1,53 @@
 import React from 'react'
 import "./Register.css"
 import { Link } from "react-router-dom"
+import LoginregisterFooter from '../../components/loginRegisterFooter/LoginregisterFooter'
 
 const Register = () => {
     return (
         <>
-            <div className="container-fluid registerPage">
-
-                <div className=" wrapper">
-                    <form className='registerFrom'>
-                        <h2>CREATE AN ACCOUNT</h2>
-                        <div className="row">
-                            <div className="col-6 leftSideLoginFrom">
-                                <input type="text" placeholder=' first  name' />
-                                <input type="text" placeholder=' username' />
-                                <input type="password" placeholder=' password' />
-
-                            </div>
-                            <div className="col-6 rightSideLoginForm">
-                                <input type="text" placeholder=' last name' />
-                                <input type="email" placeholder=' email' />
-                                <input type="text" placeholder='confirm password' />
-
-                            </div>
+            <div className=" registerPage">
+            
+                <form className='registerFrom'>
+                    <h2 className='registerTitle'>All In One</h2>
+                    <div className=" LoginFormContainer">
+                        <h4>CREATE AN ACCOUNT</h4>
+                        <div className="inputBox">
+                            <label >Your Name</label><br />
+                            <input type="text" placeholder=' first  name' />
                         </div>
-                        <div className="createBtn">
+                        <div className="inputBox">
+                            <label >Email</label><br />
+                            <input type="email" placeholder=' first  name' />
+                        </div>
+                        <div className="inputBox">
+                            <label >Phone Number</label><br />
+                            <input type="number" placeholder=' first  name' />
+                        </div>
+
+                        <div className="inputBox">
+                            <label >Password</label><br />
+                            <input type="password" placeholder=' first  name' />
+                        </div>
+                        <div className="inputBox">
                             <button>CREATE</button>
                         </div>
-                        <p>Already have an account ?
-                            {/* render to login page when click */}
-                            <Link className='link' to="/login">
-                                <span>SIGN IN</span>
-                            </Link>
-                        </p>
 
-                    </form>
-                </div>
+                    </div>
+                    <p>Already have an account ?
+                        {/* render to login page when click */}
+                        <Link className='link' to="/login">
+                            <span>SIGN IN</span>
+                        </Link>
+                    </p>
+                    <div className="terms">
+                        By creating an account, you agree to All In One's Conditions of Use and Privacy Notice.
+                    </div>
+
+                </form>
             </div>
+            {/* login and register footer */}
+            <LoginregisterFooter/>
         </>
     )
 }
