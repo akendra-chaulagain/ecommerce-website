f78b9e109e0479988dd321ba509b31b2027ab3c6
Akendra-Chaulagain <yakendrachaulagain31@gmail.com>
2022-04-18 13:05:49 +0545

    jwt

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -2,99 +2,95 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const verify = require("../verifyToken");
+
+const bodyParser = require("body-parser");
+router.use(bodyParser.json()); // for parsing application/json
+
+router.get("/", (req, res) => {
+  res.send("hello from Router");
+});
 
 // database
-require("../connection/DB");
+require("../connection/database");
 
-// user Model
-const User = require("../models/User");
-const verifyToken = require("../middleware/verifyToken");
+// usere schema and models
+const User = require("../modules/User");
 
-// register user
+// register user data
+
 router.post("/register", async (req, res) => {
-  const { username, email, number, password } = req.body;
-  if (!username || !email || !number || !password) {
-    res.status(400).json("Enter all the data");
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) {
+    return res.status(500).json("enter all the data ");
   }
   try {
-    // if email already exist in database then this function will run
+    // if  email already exist in database then this statement will run
     const userEmail = await User.findOne({ email });
     if (userEmail) {
-      return res.status(500).json("Email already exist.Please enter new email");
-      // to compare password and confirm password
-    } else {
-      const user = new User({ username, email, password, number });
-
-      // generate salt to hash password
-      const salt = await bcrypt.genSalt(12);
-      // now we set user password to hashed password
-      user.password = await bcrypt.hash(user.password, salt);
-      const token = jwt.sign(
-        { id: user._id, isAdmin: user.isAdmin },
-        process.env.JWT_SECRET_KEY,
-        { expiresIn: "1d" }
-        // { expiresIn: "1hr" }
-        // { expiresIn: "1d" }
-        // { expiresIn: "20s" }
-        // { expiresIn: "15m" }
-      );
-      // saving in cookie
-      res.cookie("jsonwebToken", token, {
-        expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 5),
-        path: "/",
-        httpOnly: true,
-        sameSite: "lax",
-      });
-
-      const result = await user.save();
-      return res.status(201).json({ success: true, result, token });
+      return res
+        .status(500)
+        .json("Email already exist. Please enter new email");
     }
+    // if email is new then this will run
+    const user = new User({ username, email, password });
+    // generate salt to hash password
+    const salt = await bcrypt.genSalt(12);
+    // now we set user password to hashed password
+    user.password = await bcrypt.hash(user.password, salt);
+    const token = jwt.sign(
+      { id: user._id, isAdmin: user.isAdmin },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: "1d" }
+    );
+    // saving in cookie
+    res.cookie("jsonwebToken", token, {
+      expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 5),
+      path: "/",
+      httpOnly: true,
+      sameSite: "lax",
+    });
+    const result = await user.save();
+    return res.status(200).json({ result, token });
   } catch (error) {
-    return res.status(400).json("unable to register data");
+    console.log(error);
   }
 });
 
-// login
+// login user data
 router.post("/login", async (req, res) => {
   const { email, password } = req.body;
-  if (!email || !password) {
-    return res.status(401).json("enter all the data");
-  }
   const user = await User.findOne({ email });
-  try {
-    // check user password with hashed password stored in the database
-    const validPassword = await bcrypt.compare(password, user.password);
+  if (user) {
+    const isMatch = await bcrypt.compare(password, user.password);
 
     const token = jwt.sign(
       { id: user._id, isAdmin: user.isAdmin },
       process.env.JWT_SECRET_KEY,
       { expiresIn: "1d" }
-      // { expiresIn: "1hr" }
-      // { expiresIn: "1d" }
-      // { expiresIn: "20s" }
-      // { expiresIn: "15m" }
     );
-
     // saving in cookie
     res.cookie("jsonwebToken", token, {
-      expires: new Date(Date.now() + 1000 * 60 * 60 * 5),
+      expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 5),
+
       path: "/",
       httpOnly: true,
       sameSite: "lax",
     });
-    if (validPassword) {
+
+    if (isMatch) {
       const { password, ...others } = user._doc;
-      return res.status(201).json({ ...others, token });
+      res.status(200).json({ ...others, token });
     } else {
-      return res.status(400).json("Invalid user data");
+      res.status(400).json({ error: "Invalid data" });
     }
-  } catch (error) {
-    return res.status(400).json("Invalid data");
+  } else {
+    res.status(401).json({ error: "User does not exist" });
   }
 });
 
 // logout user
-router.post("/logout", verifyToken, async (req, res) => {
+router.post("/logout", verify, async (req, res) => {
   const cookie = req.headers.cookie;
   if (cookie) {
     const token = cookie.split("=")[1];
